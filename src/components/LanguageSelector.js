f1630de7997356a737bd18477e8fd2b0289ab654
Rifky Niyas <63934498+MohammadRifky@users.noreply.github.com>
2023-06-18 22:32:49 +0530

    customized language selector

--- src/components/LanguageSelector.js
+++ src/components/LanguageSelector.js
@@ -1,20 +1,26 @@
 import { langs } from "@uiw/codemirror-extensions-langs";
+import Select from "react-select";
+
 const LanguageSelector = () => {
+  const languageOptions = Object.keys(langs)
+    .sort()
+    .map((lang) => ({
+      value: lang,
+      //Capitalize the first letter
+      label: lang.charAt(0).toUpperCase() + lang.slice(1),
+    }));
   return (
     <div>
       <label htmlFor="langSelect">Select Language</label>
-      <select name="langSelect" id="langSelect">
-        {Object.keys(langs)
-          .sort()
-          .map((lang) => (
-            <option value={lang}>
-              {
-                /* Capitalize the first word */
-                lang.charAt(0).toUpperCase() + lang.slice(1)
-              }
-            </option>
-          ))}
-      </select>
+      <Select
+        className="basic-single"
+        classNamePrefix="select"
+        defaultValue={languageOptions[42]} //Select JavaScript
+        isSearchable={true}
+        isClearable={true}
+        name="languageSelector"
+        options={languageOptions}
+      />
     </div>
   );
 };
