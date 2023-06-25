da7a5ff2a7761c67ff28bc507497ecc02de25392
Rifky Niyas <63934498+MohammadRifky@users.noreply.github.com>
2023-06-25 23:24:00 +0530

    restructured modal code

---
+++ src/components/Modal.js
@@ -0,0 +1,28 @@
+const Modal = ({ isOpen, onClose, children }) => {
+  if (!isOpen) return null;
+  return (
+    <>
+      <div className="justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none">
+        <div className="relative w-auto my-6 mx-auto max-w-3xl">
+          {/*content*/}
+          <div className="border-0 rounded-lg shadow-lg relative flex flex-col w-full bg-white outline-none focus:outline-none">
+            {/*header*/}
+            <div className="flex items-start justify-between p-5 border-b border-solid border-slate-200 rounded-t">
+              <h3 className="text-3xl font-semibold">Modal Title</h3>
+              <button onClick={onClose}>Close</button>
+            </div>
+            {/*body*/}
+            <div className="relative p-6 flex-auto">{children}</div>
+            {/*footer*/}
+            <div className="flex items-center justify-end p-6 border-t border-solid border-slate-200 rounded-b">
+              <button>Preview</button>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="opacity-25 fixed inset-0 z-40 bg-black"></div>
+    </>
+  );
+};
+
+export default Modal;
