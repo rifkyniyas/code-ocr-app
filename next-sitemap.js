2521d705f0b7e2b4fd9a3df21796fbe0f896147d
Rifky Niyas <63934498+MohammadRifky@users.noreply.github.com>
2023-07-29 23:57:08 +0530

    added next sitemap

---
+++ next-sitemap.js
@@ -0,0 +1,4 @@
+module.exports = {
+  siteUrl: "https://code-ocr-app.vercel.app/", // Replace with your actual website URL
+  generateRobotsTxt: true, // Optional: Include this if you want to generate a robots.txt file
+};
