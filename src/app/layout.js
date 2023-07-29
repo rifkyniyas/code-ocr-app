d2fc6d84723d599a62367b5ed35bf646763ab9f5
Rifky Niyas <63934498+MohammadRifky@users.noreply.github.com>
2023-07-29 23:32:37 +0530

    added google search id

--- src/app/layout.js
+++ src/app/layout.js
@@ -4,8 +4,10 @@ export const metadata = {
   title: "Code OCR App - Extract code from image snippets with few clicks.",
   description:
     "Found an interesting code snippet online and want to try it out asap? Extract code from images with just a few clicks and start using in your project right away. Try Code OCR App for completely free now!",
+  verification: {
+    google: process.env.GOOGLE_SEARCH_ID,
+  },
 };
-
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
